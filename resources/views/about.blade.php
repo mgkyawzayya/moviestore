aeb6776080358f3c0093a5f2af2251782a6e0e28
Kyaw Zayya <kyawzayya.ucsp@gmail.com>
2020-07-11 20:17:20 +0630

    Change Detail UI

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,7 +4,9 @@
 <div class="relative bg-black pt-2 pb-5 px-4 sm:px-6 lg:pt-10 lg:pb-2 lg:px-8">
     <div class="relative max-w-7xl mx-auto">
         <div>
-            <p class="leading-5 text-center text-2xl text-white p-6 lg:p-10 mb-2">Dr Fone Phone Service, Accessories & Movie Store</p>
+            <img class="img-fluid text-center w-28 h-28" src="{{ url('img/logo/drfone.svg') }}" alt="">
+            <p class="leading-8 text-center text-6xl text-white p-2 lg:p-10 mb-2">Dr Fone </p>
+            <p class="leading-5 text-center text-2xl text-white p-6 lg:p-10 mb-2">Phone Service, Accessories & Movie Store</p>
 
         </div>
         @endsection
--- resources/views/vendor/pagination/tailwind.blade.php
+++ resources/views/vendor/pagination/tailwind.blade.php
@@ -1,6 +1,6 @@
 @if ($paginator->hasPages())
 <nav role="navigation" aria-label="Pagination Navigation" class="flex items-center justify-between">
-    <div class="flex justify-between flex-1 lg:hidden">
+    <div class="flex justify-between flex-1 md:hidden sm:hidden">
         @if ($paginator->onFirstPage())
         <span class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-gray-800 border border-gray-800 cursor-default leading-5 rounded-md">
             {!! __('pagination.previous') !!}
