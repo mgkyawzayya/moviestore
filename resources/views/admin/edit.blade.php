734e07802703ed9f0b985c078cfc0e5df8e523f4
Kyaw Zayya <kyawzayya.ucsp@gmail.com>
2020-07-12 23:01:11 +0630

    Change Detail UI

--- resources/views/admin/edit.blade.php
+++ resources/views/admin/edit.blade.php
@@ -12,11 +12,11 @@
             <div>
                 <div class="mt-0 border-gray-200 ">
                     <div class="mt-0 sm:mt-0">
-                        <div class="sm:grid sm:grid-cols-10 sm:gap-4 sm:items-start sm:border-gray-200 sm:pt-5">
+                        <div class="sm:grid sm:grid-cols-8 sm:gap-4 sm:items-start sm:border-gray-200 sm:pt-5">
                             <label for="title" class="block text-sm font-medium leading-5 text-gray-700 sm:mt-px sm:pt-2">
                                 Title
                             </label>
-                            <div class="mt-1 sm:mt-0 sm:col-span-8">
+                            <div class="mt-1 sm:mt-0 sm:col-span-6">
                                 <div class="max-w-lg rounded-md shadow-sm">
                                     <input id="title" name="title" type="text" class="form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5" value="{{ $movie->title }}" />
                                 </div>
@@ -28,11 +28,11 @@
                         </p>
                         @enderror
 
-                        <div class="mt-1 sm:mt-1 sm:grid sm:grid-cols-10 sm:gap-4 sm:items-start sm:border-gray-200 sm:pt-5">
+                        <div class="mt-1 sm:mt-1 sm:grid sm:grid-cols-8 sm:gap-4 sm:items-start sm:border-gray-200 sm:pt-5">
                             <label for="year" class="block text-sm font-medium leading-5 text-gray-700 sm:mt-px sm:pt-2">
                                 Year
                             </label>
-                            <div class="mt-1 sm:mt-0 sm:col-span-8">
+                            <div class="mt-1 sm:mt-0 sm:col-span-6">
                                 <div class="max-w-xs rounded-md shadow-sm">
                                     <input type="number" name="year" id="year" class="form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5" value="{{ $movie->year }}" />
                                 </div>
@@ -45,11 +45,11 @@
                         </p>
                         @enderror
 
-                        <div class="mt-1 sm:mt-1 sm:grid sm:grid-cols-10 sm:gap-4 sm:items-start sm:border-gray-200 sm:pt-5">
+                        <div class="mt-1 sm:mt-1 sm:grid sm:grid-cols-8 sm:gap-4 sm:items-start sm:border-gray-200 sm:pt-5">
                             <label for="country" class="block text-sm font-medium leading-5 text-gray-700 sm:mt-px sm:pt-2">
                                 Country
                             </label>
-                            <div class="mt-1 sm:mt-0 sm:col-span-8">
+                            <div class="mt-1 sm:mt-0 sm:col-span-6">
                                 <div class="max-w-xs rounded-md shadow-sm">
                                     <select id="country" name="country" class="block form-select w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5">
                                         <option value="" {{ $movie->country == '' ? "selected" : "" }}>Choose Nation</option>
@@ -70,11 +70,11 @@
                             {{ $message }}
                         </p>
                         @enderror
-                        <div class="mt-1 sm:mt-1 sm:grid sm:grid-cols-10 sm:gap-4 sm:items-start sm:border-gray-200 sm:pt-5">
+                        <div class="mt-1 sm:mt-1 sm:grid sm:grid-cols-8 sm:gap-4 sm:items-start sm:border-gray-200 sm:pt-5">
                             <label for="code" class="block text-sm font-medium leading-5 text-gray-700 sm:mt-px sm:pt-2">
                                 Code
                             </label>
-                            <div class="mt-1 sm:mt-0 sm:col-span-8">
+                            <div class="mt-1 sm:mt-0 sm:col-span-6">
                                 <div class="max-w-xs rounded-md shadow-sm">
                                     <input id="code" name="code" class="form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5" value="{{ $movie->code }}" />
                                 </div>
@@ -87,11 +87,11 @@
                         </p>
                         @enderror
 
-                        <div class="mt-1 sm:mt-1 sm:grid sm:grid-cols-10 sm:gap-4 sm:items-start sm:border-gray-200 sm:pt-5">
+                        <div class="mt-1 sm:mt-1 sm:grid sm:grid-cols-8 sm:gap-4 sm:items-start sm:border-gray-200 sm:pt-5">
                             <label for="type" class="block text-sm font-medium leading-5 text-gray-700 sm:mt-px sm:pt-2">
                                 Type
                             </label>
-                            <div class="mt-1 sm:mt-0 sm:col-span-8">
+                            <div class="mt-1 sm:mt-0 sm:col-span-6">
                                 <div class="max-w-xs rounded-md shadow-sm">
                                     <select id="type" name="type" class="block form-select w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5">
                                         <option value="" {{ $movie->type == '' ? "selected" : "" }}>Choose Type</option>
@@ -108,11 +108,11 @@
                         </p>
                         @enderror
 
-                        <div class="mt-1 sm:mt-1 sm:grid sm:grid-cols-10 sm:gap-4 sm:items-start sm:border-gray-200 sm:pt-5">
+                        <div class="mt-1 sm:mt-1 sm:grid sm:grid-cols-8 sm:gap-4 sm:items-start sm:border-gray-200 sm:pt-5">
                             <label for="size" class="block text-sm font-medium leading-5 text-gray-700 sm:mt-px sm:pt-2">
                                 Size
                             </label>
-                            <div class="mt-1 sm:mt-0 sm:col-span-8">
+                            <div class="mt-1 sm:mt-0 sm:col-span-6">
                                 <div class="max-w-xs rounded-md shadow-sm">
                                     <input id="size" name="size" class="form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5" value="{{ $movie->size }}" maxlength="4" />
                                 </div>
@@ -125,14 +125,14 @@
                         @enderror
                         <div class="mt-1 sm:mt-1 sm:border-gray-200 pt-3">
                             <fieldset>
-                                <div class="sm:grid sm:grid-cols-10 sm:gap-4 sm:items-baseline">
+                                <div class="sm:grid sm:grid-cols-8 sm:gap-4 sm:items-baseline">
                                     <div>
                                         <legend class="text-base leading-6 font-medium text-gray-900 sm:text-sm sm:leading-5 sm:text-gray-700">
                                             Generes
                                         </legend>
                                     </div>
-                                    <div class=" sm:col-span-8">
-                                        <div class="sm:grid sm:grid-cols-8">
+                                    <div class=" sm:col-span-6">
+                                        <div class="sm:grid sm:grid-cols-7">
                                             <div class="mt-4 sm:col-span-1 xs:col-span-1">
                                                 <div class="flex items-center">
                                                     <input id="category" name="action" type="checkbox" class="form-checkbox h-4 w-4 text-indigo-600 transition duration-150 ease-in-out" value="action" {{ $movie->action == 1 ? "checked" : "" }} />
@@ -271,11 +271,11 @@
                             </fieldset>
                         </div>
 
-                        <div class="mt-1 sm:mt-1 sm:grid sm:grid-cols-10 sm:gap-4 sm:items-start sm:border-gray-200 sm:pt-5">
+                        <div class="mt-1 sm:mt-1 sm:grid sm:grid-cols-8 sm:gap-4 sm:items-start sm:border-gray-200 sm:pt-5">
                             <label for="about" class="block text-sm font-medium leading-5 text-gray-700 sm:mt-px sm:pt-2">
                                 Description
                             </label>
-                            <div class="mt-1 sm:mt-0 sm:col-span-8">
+                            <div class="mt-1 sm:mt-0 sm:col-span-6">
                                 <div class="max-w-xl flex rounded-md shadow-sm">
                                     <textarea id="description" name="description" rows="6" class="form-textarea block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5">{{ $movie->description }}</textarea>
                                 </div>
