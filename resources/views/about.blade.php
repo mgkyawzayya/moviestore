4697344fb94cb25216f15bca3689a9163576146f
Kyaw Zayya <kyawzayya.ucsp@gmail.com>
2020-07-12 11:01:45 +0630

    Change Detail UI

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,7 +5,7 @@
     <div class="relative max-w-7xl mx-auto">
         <ul class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
             <li class="col-span-1 flex flex-col text-center  rounded-lg shadow">
-                <div class="flex-1 flex flex-col p-8">
+                <div class="flex-1 flex flex-col p-14">
                     <img class="w-60 h-60 flex-shrink-0 mx-auto bg-black rounded-lg " src="{{ url('img/logo/drfone.svg') }}" alt="">
                     <h3 class="mt-8 mb-1 text-5xl text-gray-100 leading-10 font-medium">Phone Services </h3>
                     <h3 class="mt-2 mb-1 text-5xl text-gray-100 leading-10 font-medium">& </h3>
