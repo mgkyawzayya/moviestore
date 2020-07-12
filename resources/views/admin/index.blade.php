7118532029e4aaa82c7a9c89c35ca8acee5a3e3c
Kyaw Zayya <kyawzayya.ucsp@gmail.com>
2020-07-13 00:18:50 +0630

    Final Changes

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -31,6 +31,9 @@
                             <thead>
                                 <tr>
                                     <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                                        ID
+                                    </th>
+                                    <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
                                         Title
                                     </th>
                                     <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
@@ -51,6 +54,9 @@
                             <tbody class="bg-white">
                                 @foreach( $movies as $movie)
                                 <tr>
+                                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 font-medium text-gray-900">
+                                        {{ $movie->id }}
+                                    </td>
                                     <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 font-medium text-gray-900">
                                         {{ $movie->title }}
                                     </td>
